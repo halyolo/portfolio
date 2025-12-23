ef1504e5f200239ce7b032d32bc790101da4d553
halyolo <119755158+halyolo@users.noreply.github.com>
2025-12-23 13:28:30 +0900

    Reorder components in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function HomePage() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-4">
       <Header />
-      <Intro />
       <TechStack />
+      <Intro />
       <SocialLinks />
     </main>
   );
